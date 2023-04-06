77d25cba08b26ef8b676256e51a481a8ca323a93
minseokkim <altpd13@gmail.com>
2023-04-06 15:51:13 +0900

    Add MainPage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,55 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+"use client";
 
-const inter = Inter({ subsets: ['latin'] })
-
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import { BigNumber, ethers } from "ethers";
+import config from "../../config.json";
+import { getSimpleAccount } from "./api/src/getSimpleAccount";
+import { useState } from "react";
+import { formatEther } from "ethers/lib/utils";
 export default function Home() {
+  const [address, setAddress] = useState<string>("");
+  const [balance, setBalance] = useState<string>();
+
+  const createAddress = async () => {
+    const provider = new ethers.providers.JsonRpcProvider(config.rpcUrl);
+    const accountAPI = getSimpleAccount(
+      provider,
+      config.signingKey,
+      config.entryPoint,
+      config.simpleAccountFactory
+    );
+    const address = await accountAPI.getCounterFactualAddress();
+    setAddress(address);
+    console.log(`SimpleAccount address: ${address}`);
+  };
+  const getAddressBalance = async () => {
+    const provider = new ethers.providers.JsonRpcProvider(config.rpcUrl);
+    const bigNumberBalance = await provider.getBalance(address);
+    console.log(bigNumberBalance)
+    const balance = formatEther(bigNumberBalance);
+    setBalance(balance);
+  };
   return (
-    <div>Hello World</div>
-  )
+    <>
+      <div className="wallet">
+        <div>
+          <text>Your Address is {address}</text>
+        </div>
+        <div>
+          <text>Your balance is {balance}</text>
+        </div>
+        <div>
+          <button onClick={() => createAddress()}>
+            <text>Create Goerli Address</text>
+          </button>
+        </div>
+        <div>
+          <button onClick={()=> getAddressBalance()}>
+            <text>Get Goerli Balance</text>
+          </button>
+        </div>
+      </div>
+    </>
+  );
 }
