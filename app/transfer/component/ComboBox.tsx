5c3182a30bdd2d6506ac026c35c15aaf7c86e478
minseokkim <altpd13@gmail.com>
2023-05-11 01:20:04 +0900

    Add comboBox

---
+++ app/transfer/component/ComboBox.tsx
@@ -0,0 +1,100 @@
+import { Fragment, useEffect, useState } from 'react';
+import { Combobox, Transition } from '@headlessui/react';
+import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid';
+import { useSnapshot } from 'valtio';
+import TokenStore, { TokenState } from '@/store/TokenStore';
+
+
+export default function Example() {
+  const { tokenList } = useSnapshot(TokenStore.tokenListState);
+  const mainToken = useSnapshot(TokenStore.mainTokenState);
+  const [selectedToken, setSelectedToken] = useState(() =>
+    tokenList.length == 0 ? mainToken : tokenList[0]
+  );
+  const [query, setQuery] = useState('');
+  const filteredToken =
+    query === ''
+      ? tokenList
+      : tokenList.filter((token: TokenState) =>
+          token.name
+            .toLowerCase()
+            .replace(/\s+/g, '')
+            .includes(query.toLowerCase().replace(/\s+/g, ''))
+        );
+
+  return (
+    <div className="w-full">
+      <Combobox value={selectedToken} onChange={setSelectedToken}>
+        <div className="relative mt-1">
+          <div className="relative w-full cursor-default overflow-hidden rounded-lg bg-gray-700 text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
+            <Combobox.Input
+              className="w-full border-none py-2 pl-3 pr-10 text-sm leading-5 focus:ring-0"
+              displayValue={(token: TokenState) => token.name}
+              onChange={(event) => setQuery(event.target.value)}
+            />
+            {tokenList.length == 0 ? (
+              <></>
+            ) : (
+              <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
+                <ChevronUpDownIcon
+                  className="h-5 w-5 text-gray-400"
+                  aria-hidden="true"
+                />
+              </Combobox.Button>
+            )}
+
+            <div className="text-black">{selectedToken.name}</div>
+          </div>
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+            afterLeave={() => setQuery('')}
+          >
+            <Combobox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+              {filteredToken.length === 0 && query !== '' ? (
+                <div className="relative cursor-default select-none px-4 py-2 text-gray-700">
+                  Nothing found.
+                </div>
+              ) : (
+                filteredToken.map((person) => (
+                  <Combobox.Option
+                    // key={person.id}
+                    className={({ active }) =>
+                      `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                        active ? 'bg-teal-600 text-white' : 'text-gray-900'
+                      }`
+                    }
+                    value={person}
+                  >
+                    {({ selected, active }) => (
+                      <>
+                        <span
+                          className={`block truncate ${
+                            selected ? 'font-medium' : 'font-normal'
+                          }`}
+                        >
+                          {person.name}
+                        </span>
+                        {selected ? (
+                          <span
+                            className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                              active ? 'text-white' : 'text-teal-600'
+                            }`}
+                          >
+                            <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                          </span>
+                        ) : null}
+                      </>
+                    )}
+                  </Combobox.Option>
+                ))
+              )}
+            </Combobox.Options>
+          </Transition>
+        </div>
+      </Combobox>
+    </div>
+  );
+}
