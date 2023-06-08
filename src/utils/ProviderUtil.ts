e8229cc8a49bba787cf2890ff0d2e0f601e22a78
minseokkim <altpd13@gmail.com>
2023-06-08 20:22:29 +0900

    Add chainId

--- src/utils/ProviderUtil.ts
+++ src/utils/ProviderUtil.ts
@@ -4,6 +4,10 @@ export function providerOf(chainId: number) {
   switch (chainId) {
     case 80001:
       return new ethers.providers.JsonRpcProvider(process.env.NEXT_PUBLIC_MUMBAI_RPC);
+      case 420:
+      return new ethers.providers.JsonRpcProvider(process.env.NEXT_PUBLIC_OPTIMISM_GOERLI_RPC);
+      case 421613:
+      return new ethers.providers.JsonRpcProvider(process.env.NEXT_PUBLIC_ARBITRUM_GOERLI_RPC);
     default:
       return new ethers.providers.JsonRpcProvider(process.env.NEXT_PUBLIC_MUMBAI_RPC);
   }
@@ -13,7 +17,11 @@ export function projectIdOf(chainId: number) {
   switch (chainId) {
     case 80001:
       return process.env.NEXT_PUBLIC_MUMBAI_PROJECT_ID!;
+      case 420:
+      return process.env.NEXT_PUBLIC_OPTIMISM_GOERLI_PROJCET_ID!;
+      case 421613:
+      return process.env.NEXT_PUBLIC_ARBITRUM_GOERLI_PROJCET_ID!;
     default:
-      return process.env.NEXT_PUBLIC_MUMBAI_PROJECT_ID!;
+      return process.env.NEXT_PUBLIC_MUMBAI_RPC!;
   }
 }
