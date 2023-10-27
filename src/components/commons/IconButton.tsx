0e0443e53d9ef20f75c25d7addfdff53a2cbeeb5
Diana Lee <yeonylee94@gmail.com>
2023-10-27 16:14:33 +0900

    FIX: theme variable name

--- src/components/commons/IconButton.tsx
+++ src/components/commons/IconButton.tsx
@@ -9,52 +9,43 @@ import Button from './Button';
 type IconButtonSizeType = 'large' | 'medium' | 'small';
 
 interface IProps {
-    text: string;
-    icon: IconTypes;
-    type?: 'primary' | 'secondary' | 'tertiary';
-    size?: IconButtonSizeType;
-    onClick: (e?: React.MouseEvent<HTMLElement>) => void;
-    disabled?: boolean;
-    loading?: boolean;
-    style?: React.CSSProperties;
+  text: string;
+  icon: IconTypes;
+  type?: 'primary' | 'secondary' | 'tertiary';
+  size?: IconButtonSizeType;
+  onClick: (e?: React.MouseEvent<HTMLElement>) => void;
+  disabled?: boolean;
+  loading?: boolean;
+  style?: React.CSSProperties;
 }
 
 const IconButton = ({
-    text = '',
-    icon,
-    size = 'medium',
-    type = 'primary',
-    onClick = () => {},
-    disabled = false,
-    loading = false,
-    style,
-  }: IProps) => {
-
-    const renderIconColor = (): ColorType => {
-      if (disabled) return 'bg300';
-      if (loading) return 'bg100';
-      return 'bg0';
-    };
-  
-    const renderIconSize = (): SizeType => {
-      if (size === 'large') return 'body1';
-      if (size === 'small') return 'body5';
-      return 'body3'; // medium and default
-    };
-  
-    return (
-      <Button
-      text={text}
-      type={type}
-      onClick={onClick}
-      size={size}
-      style={style}
-      >
-        <Icon type={icon} color={renderIconColor()} height={renderIconSize()}/>
-      </Button>
-    );
+  text = '',
+  icon,
+  size = 'medium',
+  type = 'primary',
+  onClick = () => {},
+  disabled = false,
+  loading = false,
+  style,
+}: IProps) => {
+  const renderIconColor = (): ColorType => {
+    if (disabled) return 'bg30';
+    if (loading) return 'bg10';
+    return 'bg0';
   };
 
-  export default IconButton;
+  const renderIconSize = (): SizeType => {
+    if (size === 'large') return 'body1';
+    if (size === 'small') return 'body5';
+    return 'body3'; // medium and default
+  };
 
+  return (
+    <Button text={text} type={type} onClick={onClick} size={size} style={style}>
+      <Icon type={icon} color={renderIconColor()} height={renderIconSize()} />
+    </Button>
+  );
+};
 
+export default IconButton;
