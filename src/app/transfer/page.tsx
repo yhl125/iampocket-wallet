11619bed6e8b996e950ddfe1c8a6afdc02897d5a
MER <wjang07@gmail.com>
2023-11-09 13:55:31 +0900

    FIX : Formatting

--- src/app/transfer/page.tsx
+++ src/app/transfer/page.tsx
@@ -9,13 +9,13 @@ function TransferToken() {
     <>
       <div className="flex h-1/6 items-center justify-center border-b-2">
         <h1 className="text-2xl">Transfer</h1>
-        <button onClick={() => router.back()} className="btn-xs btn">
+        <button onClick={() => router.back()} className="btn btn-xs">
           Cancel
         </button>
       </div>
       <TransferTokenForm></TransferTokenForm>
     </>
   );
-};
+}
 
 export default TransferToken;
--- src/components/GlobalHeader.tsx
+++ src/components/GlobalHeader.tsx
@@ -4,46 +4,44 @@ import React from 'react';
 import styled from 'styled-components';
 
 import theme from '@/styles/theme';
-import {usePc} from '@/hooks/usePc';
+import { usePc } from '@/hooks/usePc';
 import Text from './commons/Text';
 import Icon from './commons/Icon';
 
-
 // MEMO: Global Navigation Header
 const Header = () => {
-
   const isPc = usePc();
 
-  console.log(isPc)
+  console.log(isPc);
   return (
- 
     <Container isPc={isPc}>
       <Left>
-          <Icon type="logo" color='brandBlue50' height={ isPc ? 'display' :'title1'}/>
-          <HeaderButtonWrapper></HeaderButtonWrapper>
+        <Icon
+          type="logo"
+          color="brandBlue50"
+          height={isPc ? 'display' : 'title1'}
+        />
+        <HeaderButtonWrapper></HeaderButtonWrapper>
       </Left>
-      <Right>
-      </Right>
+      <Right></Right>
     </Container>
-  
   );
 };
 
-const Container = styled.header< { isPc : boolean } >`
+const Container = styled.header<{ isPc: boolean }>`
   width: 100%;
   display: flex;
   align-items: center;
-  position : fixed;
-  padding: ${props => (props.isPc ? `${theme.space.medium} ${theme.space.mLarge}` : `${theme.space.small} ${theme.space.sMedium}`)};
-
+  position: fixed;
+  padding: ${(props) =>
+    props.isPc
+      ? `${theme.space.medium} ${theme.space.mLarge}`
+      : `${theme.space.small} ${theme.space.sMedium}`};
 `;
 
-
-const HeaderButtonWrapper =  styled.div`
-  gap: 40px
-
+const HeaderButtonWrapper = styled.div`
+  gap: 40px;
 `;
-
 
 const Left = styled.div`
   display: flex;
