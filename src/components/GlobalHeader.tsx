2d0bd73b73c8f107eb144ac31c82f1310cc49ff4
MER <wjang07@gmail.com>
2023-11-09 13:38:20 +0900

    FIX : header layout

--- src/components/GlobalHeader.tsx
+++ src/components/GlobalHeader.tsx
@@ -4,52 +4,51 @@ import React from 'react';
 import styled from 'styled-components';
 
 import theme from '@/styles/theme';
+import {usePc} from '@/hooks/usePc';
 import Text from './commons/Text';
 import Icon from './commons/Icon';
-import {usePc} from '@/hooks/usePc'
+
 
 // MEMO: Global Navigation Header
 const Header = () => {
-  let isPc=usePc()
+
+  const isPc = usePc();
+
+  console.log(isPc)
   return (
-    isPc
-    ?
-    <PcContainer>
+ 
+    <Container isPc={isPc}>
       <Left>
-          <Icon type="logo" color='brandBlue50' height='display'/>
-          <Text size='title2' $bold>iamPocket</Text>
+          <Icon type="logo" color='brandBlue50' height={ isPc ? 'display' :'title1'}/>
+          <HeaderButtonWrapper></HeaderButtonWrapper>
       </Left>
       <Right>
       </Right>
-    </PcContainer>
-    :
-    <MobileContainer>
-      <Left>
-          <Icon type="logo" color='brandBlue50' height='title1'/>
-          <Text size='title3' $bold>iamPocket</Text>
-      </Left>
-      <Right>
-      </Right>
-    </MobileContainer>
+    </Container>
+  
   );
 };
 
-const PcContainer = styled.header`
+const Container = styled.header< { isPc : boolean } >`
   width: 100%;
   display: flex;
   align-items: center;
   position : fixed;
-  padding: ${theme.space.medium} ${theme.space.mLarge};
+  padding: ${props => (props.isPc ? `${theme.space.medium} ${theme.space.mLarge}` : `${theme.space.small} ${theme.space.sMedium}`)};
+
 `;
 
-const MobileContainer = styled(PcContainer)`
-  padding: ${theme.space.medium} ${theme.space.base};
-`
+
+const HeaderButtonWrapper =  styled.div`
+  gap: 40px
+
+`;
+
 
 const Left = styled.div`
   display: flex;
   align-items: center;
-  gap: ${theme.space.tiny};;
+  gap: ${theme.space.tiny};
 `;
 const Right = styled.div``;
 
--- src/hooks/usePc.ts
+++ src/hooks/usePc.ts
@@ -1,10 +1,16 @@
 import { useMediaQuery } from "react-responsive"
+import { useEffect, useState } from "react";
 
 export const usePc=()=>{
-
-  const isPc = useMediaQuery({
+  
+  const [isPc, setIsPc] = useState(false);
+  const pc = useMediaQuery({
       query : "(min-width:768px)"
   });
+
+  useEffect(()=>{
+    setIsPc(pc);
+  },[pc]);
 
   return isPc
 }
