d452b2ed8d18094059b1fe4f7538a29fca6379e9
Diana Lee <yeonylee94@gmail.com>
2023-11-13 10:58:46 +0900

    FIX: input suffix case style

--- src/components/commons/Text.tsx
+++ src/components/commons/Text.tsx
@@ -43,7 +43,8 @@ const StyledText = styled.span<IProps>`
   font-weight: ${({ $bold, $thin }) => ($bold ? 700 : $thin ? 400 : 500)};
   color: ${({ color }) => theme.color[color ?? 'bg0']};
   font-size: ${({ size }) => theme.fontSize[size ?? 'body3']};
-  line-height: ${({ size }) => theme.lineHeight[size ?? 'body3']};
+  /* line-height: ${({ size }) => theme.lineHeight[size ?? 'body3']}; */
+  line-height: normal;
   text-align: ${({ $align }) => $align};
 `;
 
