d1c26a6e39860cdca4a5c9afdde1c2befb769add
minseokkim <altpd13@gmail.com>
2023-11-26 14:51:19 +0900

    add: add with paymaster option

--- src/components/swap/SwapQuoteView.tsx
+++ src/components/swap/SwapQuoteView.tsx
@@ -4,9 +4,13 @@ import { PKPState } from '@/store/PKPStore';
 import TokenStore, { IResponseToken } from '@/store/TokenStore';
 import { createPkpViemWalletClient } from '@/utils/EOAWalletUtil';
 import { erc20BalanceToReadable } from '@/utils/ERC20Util';
-import { zeroDevSigner } from '@/utils/ERC4337WalletUtil';
+import {
+  zeroDevSigner,
+  zeroDevSignerWithERC20Gas,
+} from '@/utils/ERC4337WalletUtil';
 import { IBuyTokenInfo, IPrice, IQuote, queryQuote } from '@/utils/SwapUtil';
 import { useLazyQuery } from '@apollo/client';
+import { useState } from 'react';
 import { useSnapshot } from 'valtio';
 import { encodeFunctionData } from 'viem';
 
@@ -30,7 +34,7 @@ export default function SwapQuoteView({
   chainId: number;
 }) {
   const { tokenList } = useSnapshot(TokenStore.tokenListState);
-
+  const [withPM, setWithPM] = useState(false);
   const [getQuote, { data: quoteData, loading: quoteLoading }] = useLazyQuery(
     queryQuote,
     {
@@ -56,11 +60,18 @@ export default function SwapQuoteView({
   }
 
   async function findQuoteAndSwap4337() {
-    const signer = await zeroDevSigner(
-      pkpState.currentPKP?.publicKey!,
-      pkpState.sessionSigs!,
-      price!.chainId,
-    );
+    const signer = withPM
+      ? await zeroDevSignerWithERC20Gas(
+          price?.chainId === 80001 ? 'TEST_ERC20' : 'USDC',
+          pkpState.currentPKP?.publicKey!,
+          pkpState.sessionSigs!,
+          price!.chainId,
+        )
+      : await zeroDevSigner(
+          pkpState.currentPKP?.publicKey!,
+          pkpState.sessionSigs!,
+          price!.chainId,
+        );
     const quoteResponse: IQuote = (await getQuote()).data.findSwapQuote;
     setQuote(quoteResponse);
     if (
@@ -129,6 +140,13 @@ export default function SwapQuoteView({
         Amount to Buy:
         {erc20BalanceToReadable(price!.buyAmount, buyTokenInfo!.decimals)}
       </div>
+      <div>Send With Pay Master</div>
+      <input
+        type="checkbox"
+        checked={withPM}
+        onChange={() => setWithPM(true)}
+        className="checkbox ml-0.5"
+      />
       {walletState.selectedWallet === 'zeroDev' ? (
         <button
           onClick={(e) => {
