63be79eff34f62cc9895cb3ba2e350ed531253dd
minseokkim <altpd13@gmail.com>
2023-11-27 18:16:44 +0900

    fix: fix branch conflicts

--- src/components/swap/SwapQuoteView.tsx
+++ src/components/swap/SwapQuoteView.tsx
@@ -1,3 +1,8 @@
+import { useLazyQuery } from '@apollo/client';
+import { useSnapshot } from 'valtio';
+import { encodeFunctionData } from 'viem';
+import { useRouter } from 'next/navigation';
+
 import { ERC20_ABI } from '@/abi/abi';
 import { WalletState } from '@/store/AddressStore';
 import { PKPState } from '@/store/PKPStore';
@@ -9,10 +14,14 @@ import {
   zeroDevSignerWithERC20Gas,
 } from '@/utils/ERC4337WalletUtil';
 import { IBuyTokenInfo, IPrice, IQuote, queryQuote } from '@/utils/SwapUtil';
-import { useLazyQuery } from '@apollo/client';
+
+import Text from '@/components/commons/Text';
+import Icon from '@/components/commons/Icon';
+import styled from 'styled-components';
+import theme from '@/styles/theme';
+import Button from '../commons/Button';
+import TokenImage from '../commons/TokenImage';
 import { useState } from 'react';
-import { useSnapshot } from 'valtio';
-import { encodeFunctionData } from 'viem';
 
 export default function SwapQuoteView({
   price,
@@ -35,6 +44,7 @@ export default function SwapQuoteView({
 }) {
   const { tokenList } = useSnapshot(TokenStore.tokenListState);
   const [withPM, setWithPM] = useState(false);
+  const router = useRouter();
   const [getQuote, { data: quoteData, loading: quoteLoading }] = useLazyQuery(
     queryQuote,
     {
@@ -92,6 +102,7 @@ export default function SwapQuoteView({
         ])
         .then((res) => {
           console.log(res);
+          router.push('/wallet');
         });
     } else {
       await signer
@@ -101,6 +112,7 @@ export default function SwapQuoteView({
         })
         .then((res) => {
           console.log(res);
+          router.push('/wallet');
         });
     }
   }
@@ -120,52 +132,148 @@ export default function SwapQuoteView({
         account: pkpWalletClient.account!,
         chain: pkpWalletClient.chain,
       })
-      .then((res) => {
+      .then((res: any) => {
         console.log(res);
+        router.push('/wallet');
       });
   }
-
   return (
-    <div>
-      <h1>Review Swap Order</h1>
-      <div>Sell Token Name: {sellTokenInfo?.name}</div>
-      <div>Sell Token Address: {sellTokenInfo?.address}</div>
-      <div>
-        Amount to Sell:
-        {erc20BalanceToReadable(price!.sellAmount, sellTokenInfo!.decimals)}
-      </div>
-      <div>Buy Token Name: {buyTokenInfo?.name}</div>
-      <div>Buy Token Address: {price?.buyTokenAddress}</div>
-      <div>
-        Amount to Buy:
-        {erc20BalanceToReadable(price!.buyAmount, buyTokenInfo!.decimals)}
-      </div>
-      <div>Send With Pay Master</div>
-      <input
-        type="checkbox"
-        checked={withPM}
-        onChange={() => setWithPM(true)}
-        className="checkbox ml-0.5"
-      />
+    <Container>
+      <Text size="title2" color="bg40">
+        Review Swap Order
+      </Text>
+      <ReviewBox>
+        <ReviewWrapper>
+          <TokenAmountWrapper>
+            <Text size="title3" color="bg40">
+              From
+            </Text>
+            <AmountWrapper>
+              <Text size="title1" $bold>
+                {erc20BalanceToReadable(
+                  price!.sellAmount,
+                  sellTokenInfo!.decimals,
+                )}
+              </Text>
+              <TokenWrapper>
+                <TokenImage
+                  logoUrl={sellTokenInfo?.logoUrl!}
+                  address={sellTokenInfo?.address!}
+                />
+                <Text size="title2">{sellTokenInfo?.name}</Text>
+              </TokenWrapper>
+            </AmountWrapper>
+          </TokenAmountWrapper>
+          <DividerWrapper>
+            <Divider />
+            <IconWrapper>
+              <Icon type="swap" height={40} />
+            </IconWrapper>
+          </DividerWrapper>
+          <TokenAmountWrapper>
+            <Text size="title3" color="bg40">
+              To
+            </Text>
+            <AmountWrapper>
+              <Text size="title1" $bold>
+                {erc20BalanceToReadable(
+                  price!.buyAmount,
+                  buyTokenInfo!.decimals,
+                )}
+              </Text>
+              <TokenWrapper>
+                <TokenImage logoUrl={'/PlaceHolder'} address={price!.buyTokenAddress!} />
+                <Text size="title2">{buyTokenInfo?.name}</Text>
+              </TokenWrapper>
+            </AmountWrapper>
+          </TokenAmountWrapper>
+        </ReviewWrapper>
+      </ReviewBox>
       {walletState.selectedWallet === 'zeroDev' ? (
-        <button
-          onClick={(e) => {
+        <Button
+          text="Place Order"
+          size="large"
+          type="primary"
+          onClick={(e: any) => {
             e.preventDefault();
             findQuoteAndSwap4337();
           }}
-        >
-          Place Order
-        </button>
+        />
       ) : (
-        <button
-          onClick={(e) => {
+        <Button
+          text="Place Order"
+          size="large"
+          type="primary"
+          onClick={(e: any) => {
             e.preventDefault();
             findQuoteAndSwapEOA();
           }}
-        >
-          Place Order
-        </button>
+        />
       )}
-    </div>
+    </Container>
   );
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
+
+const DividerWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 42px 0px;
+`;
+const IconWrapper = styled.div`
+  background-color: ${theme.color.bg50};
+  border-radius: 50%;
+  padding: 10px;
+  transform: rotate(-90deg);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+`;
+
+const Divider = styled.div`
+  background-color: ${theme.color.bg50};
+  height: 1px;
+  width: 100%;
+  position: relative;
+`;
+
+const ReviewBox = styled.div`
+  border-radius: 5px;
+  height: 391px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid ${theme.color.bg40};
+  padding: ${theme.space.large};
+  margin-top: ${theme.space.xSmall};
+  margin-bottom: ${theme.space.medium};
+`;
+const ReviewWrapper = styled.div`
+  width: 100%;
+`;
+
+const TokenAmountWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  row-gap: ${theme.space.small};
+`;
+
+const AmountWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const TokenWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  gap: ${theme.space.xSmall};
+`;
