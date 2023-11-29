086f7955accc8ad91044c456a206db7b2f4e0182
Yoonhyoung-Lee <54613623+Yoonhyoung-Lee@users.noreply.github.com>
2023-11-29 15:53:54 +0900

    feat: remove unused testnets

--- src/data/EIP155Data.ts
+++ src/data/EIP155Data.ts
@@ -72,36 +72,36 @@ export const EIP155_TEST_CHAINS: IEIP155ChainMap = {
     logo: '/chain-logos/eip155/137.svg',
     rpc: 'https://polygon-mumbai.infura.io/v3/5687a05736f7476ca572bd1c5a8fde8f',
   },
-  'eip155:421613': {
-    chainId: 421613,
-    name: 'Arbitrum Goerli',
-    logo: '/chain-logos/eip155/42161.svg',
-    rpc: 'https://arbitrum-goerli.infura.io/v3/5687a05736f7476ca572bd1c5a8fde8f',
-  },
-  'eip155:420': {
-    chainId: 420,
-    name: 'Optimism Goerli',
-    logo: '/chain-logos/eip155/10.svg',
-    rpc: 'https://optimism-mainnet.infura.io/v3/5687a05736f7476ca572bd1c5a8fde8f',
-  },
-  'eip155:43113': {
-    chainId: 43113,
-    name: 'Avalanche Fuji',
-    logo: '/chain-logos/eip155/43114.svg',
-    rpc: 'https://endpoints.omniatech.io/v1/avax/fuji/public',
-  },
-  'eip155:59140': {
-    chainId: 59140,
-    name: 'Linea Goerli',
-    logo: '/chain-logos/eip155/59144.svg',
-    rpc: 'https://linea-goerli.infura.io/v3/5687a05736f7476ca572bd1c5a8fde8f',
-  },
-  'eip155:1442': {
-    chainId: 1442,
-    name: 'Polygon zkEVM Testnet',
-    logo: '/chain-logos/eip155/1101.svg',
-    rpc: 'https://polygonzkevm-testnet.g.alchemy.com/v2/aeNF2qSfp9Asf8t_2EEFvWP2CS-W2bsy',
-  },
+  // 'eip155:421613': {
+  //   chainId: 421613,
+  //   name: 'Arbitrum Goerli',
+  //   logo: '/chain-logos/eip155/42161.svg',
+  //   rpc: 'https://arbitrum-goerli.infura.io/v3/5687a05736f7476ca572bd1c5a8fde8f',
+  // },
+  // 'eip155:420': {
+  //   chainId: 420,
+  //   name: 'Optimism Goerli',
+  //   logo: '/chain-logos/eip155/10.svg',
+  //   rpc: 'https://optimism-mainnet.infura.io/v3/5687a05736f7476ca572bd1c5a8fde8f',
+  // },
+  // 'eip155:43113': {
+  //   chainId: 43113,
+  //   name: 'Avalanche Fuji',
+  //   logo: '/chain-logos/eip155/43114.svg',
+  //   rpc: 'https://endpoints.omniatech.io/v1/avax/fuji/public',
+  // },
+  // 'eip155:59140': {
+  //   chainId: 59140,
+  //   name: 'Linea Goerli',
+  //   logo: '/chain-logos/eip155/59144.svg',
+  //   rpc: 'https://linea-goerli.infura.io/v3/5687a05736f7476ca572bd1c5a8fde8f',
+  // },
+  // 'eip155:1442': {
+  //   chainId: 1442,
+  //   name: 'Polygon zkEVM Testnet',
+  //   logo: '/chain-logos/eip155/1101.svg',
+  //   rpc: 'https://polygonzkevm-testnet.g.alchemy.com/v2/aeNF2qSfp9Asf8t_2EEFvWP2CS-W2bsy',
+  // },
 };
 
 export const EIP155_CHAINS: IEIP155ChainMap = {
